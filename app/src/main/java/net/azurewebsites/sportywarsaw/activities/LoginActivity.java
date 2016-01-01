9c10f2654c73a4242ebec2b18fd9c08618ead134
Marcin Chudy <marcinchudy94@gmail.com>
2016-01-01 21:32:45 +0100

    Fix back navigation in Login and Register activities

--- app/src/main/java/net/azurewebsites/sportywarsaw/activities/LoginActivity.java
+++ app/src/main/java/net/azurewebsites/sportywarsaw/activities/LoginActivity.java
@@ -148,6 +148,7 @@ public class LoginActivity extends AppCompatActivity  {
     private void showMainActivity() {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
+        finish();
     }
 }
 
