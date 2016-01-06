0601ba0228f43e0de79c0e7405f45f76ca5bbffc
kierzkowskij2 <janekkierzkowski999@gmail.com>
2016-01-06 19:22:25 +0100

    poprawiono

--- app/src/main/java/net/azurewebsites/sportywarsaw/activities/SearchFriendsActivity.java
+++ app/src/main/java/net/azurewebsites/sportywarsaw/activities/SearchFriendsActivity.java
@@ -4,15 +4,37 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import net.azurewebsites.sportywarsaw.R;
+import net.azurewebsites.sportywarsaw.adapters.SearchFacilityArrayAdapter;
+import net.azurewebsites.sportywarsaw.adapters.SearchFriendsAdapter;
+import net.azurewebsites.sportywarsaw.services.MeetingsService;
+import net.azurewebsites.sportywarsaw.services.SportsFacilitiesService;
+import net.azurewebsites.sportywarsaw.utils.DelayAutoCompleteTextView;
+
+import javax.inject.Inject;
 
 public class SearchFriendsActivity extends AppCompatActivity {
+
+    @Inject
+    MeetingsService service;
+
+    @Inject
+    SportsFacilitiesService sportsFacilitiesService;
+    private EditText stringToSearch;
+    private SearchFriendsAdapter adapter;
+    private DelayAutoCompleteTextView friendView;
+
+    private int selectedFacilityId = -1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_friends); // powinno sie ustawic
+        //setContentView(R.layout.activity_search_friends); // powinno sie ustawic
+        setContentView(R.layout.fragment_friendsfinder); // powinno sie ustawic
 
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
